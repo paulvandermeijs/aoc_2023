6d82251c7cce78026cd2b2fde694fe50a30da56f
Paul van der Meijs <vandermeijs@redkiwi.nl>
2023-12-01 21:01:22 +0100

    Day 1-2

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -1,19 +1,41 @@
 use std::io;
 
-fn main() {
-    let lines = io::stdin()
-        .lines()
-        .collect::<Result<Vec<String>, std::io::Error>>()
-        .unwrap();
+use regex::{Captures, Regex};
 
-    let sum: u32 = lines.iter().map(find_digits).sum();
+fn main() {
+    let day_regex_first = Regex::new(r"(one|two|three|four|five|six|seven|eight|nine)").unwrap();
+    let day_regex_last = Regex::new(r".*(one|two|three|four|five|six|seven|eight|nine)").unwrap();
+    let sum: u32 = io::stdin()
+        .lines()
+        .map(|line| find_digits(&day_regex_first, &day_regex_last, line.unwrap()))
+        .sum();
 
     println!("{sum}");
 }
 
-fn find_digits(line: &String) -> u32 {
-    let first = line.chars().find(|char| char.is_numeric()).unwrap();
-    let last = line.chars().rfind(|char| char.is_numeric()).unwrap();
+fn find_digits(day_regex_first: &Regex, day_regex_last: &Regex, line: String) -> u32 {
+    let replace = |caps: &Captures| match &caps[1] {
+        "one" => "1",
+        "two" => "2",
+        "three" => "3",
+        "four" => "4",
+        "five" => "5",
+        "six" => "6",
+        "seven" => "7",
+        "eight" => "8",
+        "nine" => "9",
+        _ => "0",
+    };
+    let replaced_first_line = day_regex_first.replace(&line, replace);
+    let first = replaced_first_line
+        .chars()
+        .find(|char| char.is_numeric())
+        .unwrap();
+    let replaced_last_line = day_regex_last.replace(&line, replace);
+    let last = replaced_last_line
+        .chars()
+        .rfind(|char| char.is_numeric())
+        .unwrap();
 
     let result = format!("{first}{last}").parse::<u32>().unwrap();
 
