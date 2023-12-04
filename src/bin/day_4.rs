4317f0a42e9494c162d734a12ae09185025cc783
Paul van der Meijs <vandermeijs@redkiwi.nl>
2023-12-04 19:56:36 +0100

    Day 4-2

--- src/bin/day_4.rs
+++ src/bin/day_4.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let result: u32 = std::io::stdin()
-        .lines()
-        .map(|l| l.unwrap())
-        .map(|l| {
+    let (result, _) = std::io::stdin().lines().map(|l| l.unwrap()).fold(
+        (0, Vec::<u32>::new()),
+        |(count, copies), l| {
+            let inc = 1 + copies.len();
             let (left, right) = l.split_once(':').unwrap().1.split_once('|').unwrap();
 
             let left = left
@@ -11,19 +11,28 @@ fn main() {
                 .map(|v| v.parse::<u32>().unwrap())
                 .collect::<Vec<u32>>();
 
-            let score = right
+            let matches = right
                 .split(' ')
                 .filter(|v| !v.is_empty())
                 .map(|v| v.parse::<u32>().unwrap())
                 .filter(|v| left.contains(v))
-                .fold(0, |f, _| match f {
-                    0 => 1,
-                    _ => f * 2,
-                });
+                .count();
 
-            score
-        })
-        .sum();
+            let mut copies = copies
+                .into_iter()
+                .map(|v| v - 1)
+                .filter(|v| *v > 0)
+                .collect::<Vec<u32>>();
+
+            if matches > 0 {
+                for _ in 0..inc {
+                    copies.push(matches as u32);
+                }
+            }
+
+            (count + inc, copies)
+        },
+    );
 
     println!("{:?}", result);
 }
